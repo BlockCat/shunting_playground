096a3f636bf16bcd19337c3e3f4ec7541d1c88d4
BlockCat <zinoonomiwo@gmail.com>
2022-10-24 12:29:58 +0200

    start with solution

--- yard-viz-bevy/src/main.rs
+++ yard-viz-bevy/src/main.rs
@@ -1,5 +1,3 @@
-use std::time::{Duration, Instant, SystemTime};
-
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext, EguiPlugin};
 use bevy_inspector_egui::WorldInspectorPlugin;
@@ -7,6 +5,7 @@ use bevy_mod_picking::DefaultPickingPlugins;
 use bevy_prototype_lyon::prelude::*;
 use camera_plugin::CameraPlugin;
 use rail_view::RailViewPlugin;
+use std::time::Duration;
 mod camera_plugin;
 mod rail_view;
 mod yard_plugin;
@@ -26,20 +25,17 @@ fn main() {
 }
 
 fn ui_example(mut egui_context: ResMut<EguiContext>) {
-    egui::Window::new("Time")
-        .default_width(5000.0)
-        .min_width(5000.0)
-        .show(egui_context.ctx_mut(), |ui| {
-            ui.add(
-                egui::Slider::new(&mut 0u64, 0..=24 * 60 * 60u64).custom_formatter(|d: f64, _| {
-                    let duration = Duration::from_secs(d as u64);
-                    format!(
-                        "{}:{}:{}",
-                        duration.as_secs() / 3600,
-                        (duration.as_secs() % 3600) / 60,
-                        duration.as_secs() % 60
-                    )
-                }),
-            );
-        });
+    egui::Window::new("Time").show(egui_context.ctx_mut(), |ui| {
+        ui.add(
+            egui::Slider::new(&mut 0u64, 0..=24 * 60 * 60u64).custom_formatter(|d: f64, _| {
+                let duration = Duration::from_secs(d as u64);
+                format!(
+                    "{}:{}:{}",
+                    duration.as_secs() / 3600,
+                    (duration.as_secs() % 3600) / 60,
+                    duration.as_secs() % 60
+                )
+            }),
+        );
+    });
 }
